84468f03e7a881352d6ee2511e47b554c216c636
eksborgs <eksborgs@gmail.com>
2026-07-31 04:33:57 +0800

    Update build.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,12 +1,44 @@
-plugins {
-    kotlin("jvm") version "1.9.20"
+buildscript {
+    repositories {
+        google()
+        mavenCentral()
+        maven("https://jitpack.io")
+    }
+    dependencies {
+        classpath("com.android.tools.build:gradle:7.4.2")
+        classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:1.8.22")
+        classpath("com.github.recloudstream:gradle:master-SNAPSHOT")
+    }
+}
+
+apply(plugin = "com.android.library")
+apply(plugin = "kotlin-android")
+apply(plugin = "com.lagradost.cloudstream3.gradle")
+
+android {
+    compileSdk = 33
+
+    defaultConfig {
+        minSdk = 21
+    }
+
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_1_8
+        targetCompatibility = JavaVersion.VERSION_1_8
+    }
+
+    kotlinOptions {
+        jvmTarget = "1.8"
+    }
 }
 
 repositories {
+    google()
     mavenCentral()
     maven("https://jitpack.io")
 }
 
 dependencies {
-    implementation("com.github.recloudstream:cloudstream:master-SNAPSHOT")
+    val cloudstream = "master-SNAPSHOT"
+    implementation("com.github.recloudstream:cloudstream:$cloudstream")
 }
