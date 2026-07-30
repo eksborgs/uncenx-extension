b0d2d7a9f54bd094965d7f14b5a9c30ea783ba9c
eksborgs <eksborgs@users.noreply.github.com>
2026-07-31 06:25:20 +0800

    Fix: Precise single-parameter reflection setter for Cloudstream repoUrl

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,3 +1,5 @@
+import com.android.build.gradle.LibraryExtension
+
 buildscript {
     repositories {
         google()
@@ -7,7 +9,7 @@ buildscript {
     dependencies {
         classpath("com.android.tools.build:gradle:7.4.2")
         classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:1.8.22")
-        classpath("com.github.recloudstream:gradle:3.0.1")
+        classpath("com.github.recloudstream:gradle:master-SNAPSHOT")
     }
 }
 
@@ -15,11 +17,29 @@ apply(plugin = "com.android.library")
 apply(plugin = "kotlin-android")
 apply(plugin = "com.lagradost.cloudstream3.gradle")
 
-cloudstream {
-    setRepoUrl("https://github.com/eksborgs/uncenx-extension")
+// Penapisan refleksif tepat: Wajibkan 1 parameter String sahaja
+val cloudstreamExt = extensions.findByName("cloudstream")
+if (cloudstreamExt != null) {
+    val targetUrl = "https://github.com/eksborgs/uncenx-extension"
+    val repoMethod = cloudstreamExt.javaClass.methods.firstOrNull { method ->
+        method.parameterTypes.size == 1 && 
+        method.parameterTypes[0] == String::class.java && 
+        (method.name.startsWith("set") || method.name.contains("repo", ignoreCase = true))
+    }
+    
+    if (repoMethod != null) {
+        repoMethod.invoke(cloudstreamExt, targetUrl)
+    } else {
+        cloudstreamExt.javaClass.declaredFields.firstOrNull { 
+            it.name.contains("repo", ignoreCase = true) 
+        }?.let { field ->
+            field.isAccessible = true
+            field.set(cloudstreamExt, targetUrl)
+        }
+    }
 }
 
-android {
+configure<LibraryExtension> {
     compileSdk = 33
 
     defaultConfig {
@@ -30,19 +50,10 @@ android {
         sourceCompatibility = JavaVersion.VERSION_17
         targetCompatibility = JavaVersion.VERSION_17
     }
-
-    kotlinOptions {
-        jvmTarget = "17"
-    }
-}
-
-repositories {
-    google()
-    mavenCentral()
-    maven { url = uri("https://jitpack.io") }
 }
 
 dependencies {
-    implementation("com.github.recloudstream:cloudstream:3.0.1")
+    val implementation by configurations
+    implementation("com.github.recloudstream:cloudstream:master-SNAPSHOT")
     implementation("org.jsoup:jsoup:1.15.4")
 }
